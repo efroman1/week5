1cfaf12c87a255fb0fb08374d1d9e2ea48ef3ee5
Emily <efroman1@student.cscc.edu>
2016-12-13 20:19:28 -0500

    Project3

--- src/com/emilyfroman/Project2/Main.java
+++ src/com/emilyfroman/Project2/Main.java
@@ -1,45 +1,92 @@
 package com.emilyfroman.Project2;
 /*
  created by Emily Froman
- 11/26/16
+ 12/10/16
  */
+
+import java.io.*;
+import java.util.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
-class Task
-{
-    //variables
+class TaskList implements Iterable<Task> {
+    public ArrayList<Task> taskList = new ArrayList<Task>();
+    public void add(Task t) {
+        taskList.add (t);
+    }
+    public void add(int i, Task t) {
+        taskList.add (i, t);
+    }
+    public void remove(int number) {
+        taskList.remove (number);
+    }
+    public Integer getSize() {
+        return taskList.size();
+    }
+    public int getIndex(Task t){
+        return taskList.indexOf(t);
+    }
+    @Override
+    public Iterator<Task> iterator() {
+        return taskList.iterator();
+    }
+    public int getPriority(Task t){
+        return t.priority;
+    }
+
+    public void sort() {
+        Collections.sort(taskList);
+    }
+    public void clear () {
+        taskList.clear();
+    }
+}
+class Task implements Comparable {
+    // title, description, and priority
     String title;
     String description;
     int priority;
-
-    Task()
-    {
+    Task(){
         title = Main.getStringKeyInput("Enter the new task's name.");
         description = Main.getStringKeyInput("Enter the new task's description.");
         priority = Main.getPriorityIntegerKeyInput("Enter the new task's priority");
     }
-
-    public int getPriority()
-    {
+    Task(String title, String description, int priority){
+        this.title = title;
+        this.description = description;
+        this.priority = priority;
+    }
+    public int getPriority() {
         return priority;
     }
 
-    void display()
-    {
-        System.out.println("Title: " + title + " Description: " + description + " Priority: " + priority);
-
+    void display(){
+        System.out.printf ("Title: " + title + " ");
+        System.out.printf("Description: " + description + " " );
+        System.out.println("Priority: " + priority);
     }
-}
 
-public class Main
-{
+    @Override
+    public int compareTo(Object o) {
+        Task task = (Task) o;
+        if (priority != task.priority)
+            return (priority - task.priority);
+        else
+            return (title.compareTo(task.title));
+    }
+public class Main {
     static Scanner scanner = new Scanner(System.in);
     static String keyInput;
+    public static int MAXKEYINPUT = 8;
+    public static int MAXPRIORITY = 5;
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         ArrayList<Task> taskArray = new ArrayList<>();
         boolean done = false;
         int input;
@@ -49,19 +96,31 @@ public class Main
             input = getMenuInput("Menu");
             if (input == 0) {
                 break;
-            }
-            else {
-                switch (input)
-                {
-                    case 1: taskArray = addTask(taskArray);
+            } else {
+                switch (input) {
+                    case 1:
+                        addTask(taskList);
                         break;
-                    case 2: taskArray = removeTask(taskArray);
+                    case 2:
+                        removeTask(taskList);
                         break;
-                    case 3: taskArray = updateTask(taskArray);
+                    case 3:
+                        updateTask(taskList);
                         break;
-                    case 4: listAllTasks(taskArray);
+                    case 4:
+                        listAllTasks(taskList);
                         break;
-                    case 5: listPriorityNumberTasks(taskArray);
+                    case 5:
+                        listPriorityNumberTasks(taskList);
+                        break;
+                    case 6:
+                        taskList.sort();
+                        break;
+                    case 7:
+                        writeListToFile(taskList);
+                        break;
+                    case 8:
+                        readListFromFile(taskList);
                         break;
                     case 0:
                         break;
@@ -70,128 +129,192 @@ public class Main
         }
     }
 
-    public static void displayMenu()
-    {
+    private static void writeListToFile(TaskList taskList) {
+
+        try {
+            File file = new File("tasklist.txt");
+
+            if (!file.exists()) {
+                file.createNewFile();
+            } else {
+                file.delete();
+                file.createNewFile();
+            }
+            FileWriter fileWriter = new FileWriter(file.getName(), true);
+            BufferedWriter bufferWriter = new BufferedWriter(fileWriter);
+            String newContent;
+            for (Task t : taskList) {
+                newContent = t.title + "::" + t.description + "::" + t.priority + System.lineSeparator();
+                ;
+                bufferWriter.write(newContent);
+            }
+            bufferWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static void readListFromFile(TaskList taskList) {
+
+        if (taskList.getSize() != 0) {
+            taskList.clear();
+        }
+        try {
+            File file = new File("tasklist.txt");
+
+            if (!file.exists()) {
+                return;
+            }
+            FileReader fileReader = new FileReader(file.getName());
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String newContent;
+
+            while ((newContent = bufferedReader.readLine()) != null) {
+
+                String[] tokens = newContent.split("::");
+                int i = 0;
+                String title = "", description = "";
+                int priority = 0;
+                for (String token : tokens) {
+                    switch (i) {
+                        case 0:
+                            title = token;
+                            break;
+                        case 1:
+                            description = token;
+                            break;
+                        case 2:
+                            priority = Integer.parseInt(token);
+                            break;
+                    }
+                    i++;
+                }
+                Task t = new Task(title, description, priority);
+                taskList.add(t);
+            }
+            bufferedReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void displayMenu() {
         System.out.println("Please choose an option:");
         System.out.println("1) Add a task.");
         System.out.println("2) Remove a task.");
         System.out.println("3) Update a task.");
         System.out.println("4) List all tasks.");
         System.out.println("5) List all tasks of a certain priority.");
+        System.out.println("6) Sort all tasks.");
+        System.out.println("7) Write tasks to File.");
+        System.out.println("8) Read tasks from File.");
         System.out.println("0) Exit.");
     }
 
-    public static ArrayList<Task> addTask(ArrayList<Task> taskArray)
-    {
+    public static void addTask(TaskList taskList) {
         Task task = new Task();
         taskArray.add(task);
-        return taskArray;
+
     }
-    public static ArrayList<Task> removeTask(ArrayList<Task> taskArray)
-    {
+
+    public static void removeTask(TaskList taskList) {
         int taskNumber;
         taskNumber = getAnyIntegerKeyInput("Task to remove: ");
-        if (taskNumber < taskArray.size())
+        if (taskNumber < taskList.getSize())
             taskArray.remove(taskNumber);
-        return taskArray;
+
     }
-    public static ArrayList<Task> updateTask(ArrayList<Task> taskArray)
-    {
+
+    public static void updateTask(TaskList taskList) {
         int taskNumber;
         taskNumber = getAnyIntegerKeyInput("Task to update: ");
-        if (taskNumber < taskArray.size()) {
-            taskArray.remove (taskNumber);
+        if (taskNumber < taskList.getSize()) {
+            taskArray.remove(taskNumber);
             Task task = new Task();
             taskArray.add(taskNumber, task);
         }
-        return taskArray;
+
     }
 
-    public static void listAllTasks(List<Task> taskArray)
-    {
-        int last;
-        last = taskArray.size();
-        for (int i=0; i < last; i++)
-        {
-            System.out.printf("Task: " + i + ", ");
-            taskArray.get(i).display();
+    public static void listAllTasks(TaskList taskList) {
+        Task task;
+
+        for (Object t : taskList) {
+            System.out.printf("Task index: " + taskList.getIndex(task) + ", ");
+            taskArray.display();
         }
     }
 
-    public static void listPriorityNumberTasks(List<Task> taskArray)
-    {
+    public static void listPriorityNumberTasks(TaskList taskList) {
         int priority;
-        int last;
+        Task task;
 
         priority = getPriorityIntegerKeyInput("Enter a priority");
-        last = taskArray.size();
-        for (int i=0; i < last; i++)
-        {
-            if (taskArray.get(i).getPriority() == priority){
-                System.out.printf("Task: " + i + ", ");
-                taskArray.get(i).display();
+
+        for (Object t : taskList) {
+            if (taskArray.getPriority(task) == priority) {
+                System.out.printf("Task index: " + taskList.getIndex(task) + ", ");
+                taskArray.display();
             }
         }
     }
-    public static int getAnyIntegerKeyInput(String promptMessage)
-    {
+
+    public static int getAnyIntegerKeyInput(String promptMessage) {
         int intInput = 0;
         boolean done = false;
-        while (!done){
-            try{
+        while (!done) {
+            try {
                 System.out.println(promptMessage);
                 keyInput = scanner.nextLine();
                 intInput = Integer.parseInt(keyInput);
                 done = true;
-            }
-            catch (NumberFormatException e){
+            } catch (NumberFormatException e) {
                 System.out.println("Must enter integer try again");
                 keyInput = scanner.nextLine();
             }
         }
-        return intInput;
+        return (intInput);
     }
-    public static int getPriorityIntegerKeyInput(String message)
-    {
+
+    public static int getPriorityIntegerKeyInput(String message) {
         int intInput = 0;
         boolean done = false;
-        while (!done){
+        while (!done) {
             intInput = getAnyIntegerKeyInput(message);
-            if ((intInput < 0) || (intInput > 5)) {
-                System.out.println("Priority must be between 0 and 5");
-            }
-            else
+            if ((intInput < 0) || (intInput > MAXKEYINPUT)) {
+                System.out.println("Priority must be between 0 and " + MAXKEYINPUT);
+            } else
                 done = true;
         }
         return (intInput);
     }
-    public static int getMenuInput(String message)
-    {
+
+    public static int getMenuInput(String message) {
         int intInput = 0;
         boolean done = false;
-        while (!done){
+        while (!done) {
             intInput = getAnyIntegerKeyInput(message);
-            if ((intInput < 0) || (intInput > 5)) {
-                System.out.println("Must enter between 0 and 5");
-            }
-            else
+            if ((intInput < 0) || (intInput > MAXKEYINPUT)) {
+                System.out.println("Must enter between 0 and " + MAXKEYINPUT);
+            } else
                 done = true;
         }
         return (intInput);
     }
-    public static String getStringKeyInput(String message)
-    {
+
+    public static String getStringKeyInput(String message) {
         boolean done = false;
-        while (!done){
+        while (!done) {
             System.out.println(message);
             keyInput = scanner.nextLine();
             if (keyInput == null) {
                 System.out.println("Must enter a value");
-            }
-            else
+            } else
                 done = true;
         }
         return keyInput;
     }
+}}
 
-}
+
